96d093f700adacba833d589ba2a13a91d38de8f5
최윤석 <97040581+Yundae203@users.noreply.github.com>
2024-10-13 14:02:53 +0900

    Project Init

---
+++ src/test/java/enterprise/subject/SubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package enterprise.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
